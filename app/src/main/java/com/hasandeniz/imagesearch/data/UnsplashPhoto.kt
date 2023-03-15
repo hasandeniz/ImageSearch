206995c896a88ef93798799cbd6ac518ee741506
Hasan Deniz <hasandeniz394@gmail.com>
2023-03-15 13:16:46 +0300

    created the main app structure and started creating main fragment. also created the main model class for data which will come from api

---
+++ app/src/main/java/com/hasandeniz/imagesearch/data/UnsplashPhoto.kt
@@ -0,0 +1,29 @@
+package com.hasandeniz.imagesearch.data
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String
+    ) : Parcelable {
+        val attributionUrl get() = "https://unsplash.com/$username?utm_source=ImageSearch&utm_medium=referral"
+    }
+}
---
+++ app/src/main/java/com/hasandeniz/imagesearch/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,7 @@
+package com.hasandeniz.imagesearch.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.hasandeniz.imagesearch.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+}
